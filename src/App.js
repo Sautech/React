5fe0fb5869643ea97aee3e3be4bbc9cc757a46c2
Sautech <saurabh.anil12@gmail.com>
2021-10-05 19:21:41 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-
-import './App.css';
-
-function App()
-{
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+function App() {
   return (
-    <div>
-      <p>Hi How are you?</p>
-      <p>Hi How are you?</p>
+    <div className="App">
+      <h1>Routing setup</h1>
+      <Router>
+        <Link to="/home"> Home Page </Link> <br></br>
+        <Link to="/about"> About Page </Link>
+        <Home />
+        <About />
+      </Router>
     </div>
   );
 }
 
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h1>This is a home page</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <h1>This is a about page</h1>
+    </div>
+  );
+}
 export default App;
