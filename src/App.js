a2f9e69752f83654f7b3686c74c205788fd25ce2
Sautech <saurabh.anil12@gmail.com>
2021-10-05 19:58:17 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import Nav from './Nav';
+import { Route } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import About from "./About";
 function App() {
   return (
     <div className="App">
       <h1>Routing setup</h1>
-      <Nav/>
+      <Nav />
       <Route path="/home">
         <Home />
       </Route>
       <Route path="/about">
         <About />
       </Route>
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <h1>This is a home page</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <h1>This is a about page</h1>
     </div>
   );
 }
